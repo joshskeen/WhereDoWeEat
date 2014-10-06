a95d37888617b63fc3b35bdd6481c47e912ee470
Josh Skeen <josh@joshskeen.com>
2014-10-06 09:41:34 -0400

    remove extra behaviorsubject

--- app/src/main/java/com/joshskeen/wheredoweeat/service/YelpServiceManager.java
+++ app/src/main/java/com/joshskeen/wheredoweeat/service/YelpServiceManager.java
@@ -3,32 +3,21 @@ package com.joshskeen.wheredoweeat.service;
 import com.joshskeen.wheredoweeat.model.Business;
 import com.joshskeen.wheredoweeat.model.LocationProvider;
 
-import retrofit.RetrofitError;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
-import rx.subjects.BehaviorSubject;
 
-/**
- * Created by joshskeen on 10/3/14.
- */
 public class YelpServiceManager {
 
     private YelpService mYelpService;
-    private BehaviorSubject<RetrofitError> mErrorBehaviorSubject = BehaviorSubject.create();
 
     public YelpServiceManager(YelpService yelpService, LocationProvider locationProvider) {
         mYelpService = yelpService;
-        mYelpService.setBehaviorSubject(mErrorBehaviorSubject);
         mYelpService.setLocationProvider(locationProvider);
     }
 
-    public BehaviorSubject<RetrofitError> getErrorBehaviorSubject() {
-        return mErrorBehaviorSubject;
-    }
-
     public Observable<Business> performSearch() {
-        return mYelpService.search("pizza")
+        return mYelpService.search("food")
                 .subscribeOn(Schedulers.newThread()) //specify which Scheduler an Observable should use when its subscription is invoked
                 .observeOn(AndroidSchedulers.mainThread()) //specify on which Scheduler a Subscriber should observe the Observable
                 .flatMap(searchResponse -> Observable.from(searchResponse.mBusinesses))
